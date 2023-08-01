45c2119ae756034e8a4af05201bfa1f2cb348644
edkim3275 <edkim3275@gmail.com>
2023-08-01 20:53:19 +0900

    feat : page layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,14 @@
 import type { AppProps } from "next/app";
+import "../styles/common.scss";
+import { Footer } from "src/components/organisms/Footer";
+import { Header } from "src/components/organisms/Header";
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }: AppProps) => (
+  <div>
+    <Header />
+    <Component {...pageProps} />
+    <Footer />
+  </div>
+);
 
 export default App;
