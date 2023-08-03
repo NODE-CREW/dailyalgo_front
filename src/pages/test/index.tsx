60fb2c73d43b04d8d46d7fa508f97534a516556c
edkim3275 <edkim3275@gmail.com>
2023-08-03 23:02:14 +0900

    feat : 임시 test 페이지 확인

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,17 +1,23 @@
-import { ChangeEvent, useState } from "react";
-import Input from "src/components/common/Input/Input";
+import { useState } from "react";
+import { Button } from "src/components/atoms/Button";
+import { Input } from "src/components/atoms/Input";
+// import Input from "src/components/common/Input/Input";
 
 const Test = () => {
   const [value, setValue] = useState("");
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    console.log(e);
-    setValue(() => e.target.value);
-  };
+  // const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  //   console.log(e);
+  //   setValue(() => e.target.value);
+  // };
   return (
-    <div>
-      this is test page
-      <input type="text" value={value} onChange={handleChange} />
-      <Input type="text" inputValue={value} setInputValue={setValue} />
+    <div style={{ marginTop: 150, height: "100vh" }}>
+      <Button className="normal">normal</Button>
+      <Button className="round">round</Button>
+      <Button className="lg">long</Button>
+
+      <Input value={value} setValue={setValue} />
+      {/* <input type="text" value={value} onChange={handleChange} /> */}
+      {/* <Input type="text" inputValue={value} setInputValue={setValue} /> */}
     </div>
   );
 };
