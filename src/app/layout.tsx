d83be40e09631ff87860d4d29b4bfd34059175df
edkim3275 <edkim3275@gmail.com>
2023-08-09 04:07:53 +0900

    feat : _app next.js13으로 마이그레이션

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,25 @@
 import { ReactNode } from "react";
-
+import "../styles/common.scss";
+// import { Footer } from "src/components/organisms/Footer";
+// import { Header } from "src/components/organisms/Header";
 interface Props {
   children: ReactNode;
 }
 
 const RootLayout = ({ children }: Props) => (
   <html lang="ko">
-    <head />
-    <body>{children}</body>
+    <head>
+      <title>HOME | DAILY ALGO</title>
+      <meta name="description" content="데일리알고 메인 홈입니다." />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="stylesheet" href="/src/styles/common.scss" />
+    </head>
+    <body>
+      {/* <Header /> */}
+      {children}
+      {/* <Footer /> */}
+    </body>
   </html>
 );
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
-const Pages = () => <div>home</div>;
+const Pages = () => <div style={{ marginTop: 60 }}>DAILY ALGO</div>;
 
 export default Pages;
