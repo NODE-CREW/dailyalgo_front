dbd4f73ee9cc980331df0b6094b761b865f0c491
edkim3275 <edkim3275@gmail.com>
2023-08-15 23:18:12 +0900

    css : global css, checkbox 이미지추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import "../styles/common.scss";
+import "./global.scss";
 // import { Footer } from "src/components/organisms/Footer";
 // import { Header } from "src/components/organisms/Header";
 interface Props {
@@ -13,7 +13,6 @@ const RootLayout = ({ children }: Props) => (
       <meta name="description" content="데일리알고 메인 홈입니다." />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
-      <link rel="stylesheet" href="/src/styles/common.scss" />
     </head>
     <body>
       {/* <Header /> */}
