d6a4c89cd2fb8da6bedf068c866b677e2db34a2b
hyelim <hallov012@gmail.com>
2024-01-16 01:58:53 +0900

    feat: mypage alarm

--- src/components/MyPage/MyPageContentForm/NotificationList/NotificationList.tsx
+++ src/components/MyPage/MyPageContentForm/NotificationList/NotificationList.tsx
@@ -1,20 +1,51 @@
 import classNames from "classnames/bind";
+import { fetchNotificationList } from "src/api/Notification";
+import { useState, useEffect } from "react";
+import type { Notification } from "src/types/notification";
+import { toast } from "react-toastify";
+import { Pagination } from "@components/common/Pagination/Pagination";
 import { NotificationListItem } from "../NotificationListItem";
 import style from "./NotificationList.module.scss";
 
 const cx = classNames.bind(style);
 
 const NotificationList = () => {
+  const [notificationList, setNotificationList] = useState<Notification[]>([]);
+  const [totalCnt, setTotalCnt] = useState<number>(0);
+  const [page, setPage] = useState<number>(1);
+
+  useEffect(() => {
+    const getNotificationList = async () => {
+      const offset = (page - 1) * 10;
+
+      const requestBody = {
+        unreadOnly: false,
+        offset,
+      };
+
+      try {
+        const res = await fetchNotificationList(requestBody);
+        setTotalCnt(res.total_cnt);
+        setNotificationList(res.notification_list);
+      } catch (e) {
+        toast.error("예기치 못한 오류가 발생했습니다. 나중에 다시 시도해주세요.");
+      }
+    };
+
+    getNotificationList();
+  }, [page]);
+
   return (
     <div className={cx("notification-wrap")}>
       <div className={cx("notification-header")}>
         <span className={cx("notification-header-text")}>알림</span>
       </div>
       <div className={cx("notification-list")}>
-        <NotificationListItem />
-        <NotificationListItem />
-        <NotificationListItem />
+        {notificationList.map((notification) => (
+          <NotificationListItem key={notification.id} notification={notification} />
+        ))}
       </div>
+      <Pagination totalCnt={totalCnt} page={page} setPage={setPage} />
     </div>
   );
 };
--- src/components/MyPage/MyPageContentForm/NotificationListItem/NotificationListItem.tsx
+++ src/components/MyPage/MyPageContentForm/NotificationListItem/NotificationListItem.tsx
@@ -1,21 +1,148 @@
 import classNames from "classnames/bind";
+import type { Notification } from "src/types/notification";
+import { TimeAgo } from "@components/user/TimeAgo";
 import style from "./NotificationListItem.module.scss";
+import { fetchNotificationItem } from "src/api/Notification";
 
 const cx = classNames.bind(style);
 
-const NotificationListItem = () => {
+interface Props {
+  notification: Notification;
+}
+
+const NotificationListItem = ({ notification }: Props) => {
+  const getObjectNameString = (text: string) => {
+    const MAX_LENGTH = 20;
+    if (text.length > MAX_LENGTH) return `"${text.slice(0, MAX_LENGTH)}..."`;
+    return `"${text}"`;
+  };
+
+  const getNotificationContent = () => {
+    const objectType = notification.object;
+    const typeName = notification.type;
+
+    const subjectName = notification.subject_name;
+    const objectName = getObjectNameString(notification.object_name);
+
+    switch (objectType) {
+      case "question":
+        switch (typeName) {
+          case "answer":
+            return (
+              <p>
+                <span>{subjectName}</span>님이 회원님의 <span>{objectName}</span> 질문에 답변을
+                달았습니다.
+              </p>
+            );
+
+          case "like":
+            return (
+              <p>
+                <span>{subjectName}</span>님이 회원님의 <span>{objectName}</span> 질문에 좋아요를
+                눌렀습니다.
+              </p>
+            );
+
+          default:
+            return null;
+        }
+
+      case "question_comment":
+        switch (typeName) {
+          case "comment":
+            return (
+              <p>
+                <span>{subjectName}</span>님이 회원님의 <span>{objectName}</span>질문에 댓글을
+                남겼습니다.
+              </p>
+            );
+          case "like":
+            return (
+              <p>
+                <span>{subjectName}</span>님이 회원님의 댓글에 좋아요를 눌렀습니다.
+              </p>
+            );
+
+          default:
+            return null;
+        }
+
+      case "answer":
+        switch (typeName) {
+          case "answer":
+            return (
+              <p>
+                <span>{subjectName}</span>님이 회원님의 <span>{objectName}</span>질문에 답변을
+                달았습니다.
+              </p>
+            );
+          case "like":
+            return (
+              <p>
+                <span>{subjectName}</span>님이 회원님의 답변에 좋아요를 눌렀습니다.
+              </p>
+            );
+
+          default:
+            return null;
+        }
+
+      case "answer_comment":
+        switch (typeName) {
+          case "comment":
+            return (
+              <p>
+                <span>{subjectName}</span>님이 회원님의 답변에 댓글을 남겼습니다.
+              </p>
+            );
+
+          case "like":
+            return (
+              <p>
+                <span>{subjectName}</span>님이 회원님의 댓글에 좋아요를 눌렀습니다.
+              </p>
+            );
+
+          default:
+            return null;
+        }
+
+      case "user":
+        switch (typeName) {
+          case "follow":
+            return (
+              <p>
+                <span>{subjectName}</span>님이 회원님을 팔로우합니다.
+              </p>
+            );
+
+          default:
+            return null;
+        }
+
+      default:
+        return null;
+    }
+  };
+
+  const clickNotificationHandler = async () => {
+    try {
+      await fetchNotificationItem(notification.id);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <div className={cx("notification-item")}>
+    <div onClick={clickNotificationHandler} className={cx("notification-item")}>
       <div className={cx("notification-item-top")}>
-        <div className={cx("notification-top-content")}>
-          <span className={cx("notification-highlight")}>블라블라</span>님이 회원님의 "
-          <span className={cx("notification-highlight")}>제목일부분...</span>"컨텐츠에 댓글을
-          달았습니다
-        </div>
-        <span className={cx("notification-time")}>50분전</span>
+        <div className={cx("notification-top-content")}>{getNotificationContent()}</div>
+        <span className={cx("notification-time")}>
+          <TimeAgo time={notification.created_time} />
+        </span>
       </div>
       <div className={cx("notification-item-bottom")}>
-        <span>재밋는 질문이네요! 해당 문제는 이렇게 풀어보면 어떨까요? 개...</span>
+        {notification.content && <span>{notification.content}</span>}
       </div>
     </div>
   );
