7bbf15120b87199b651ff6bf83ea268176d4f1d5
hyelim <hallov012@gmail.com>
2024-01-23 00:10:02 +0900

    upload

--- src/lib/http-client/index.ts
+++ src/lib/http-client/index.ts
@@ -16,12 +16,38 @@ const requestInterceptor: RequestInterceptor = {
   },
 };
 
+let isRefreshing = false;
+let refreshQueue: any[] = [];
+
 const responseInterceptor: ResponseInterceptor = {
   async onFulfilled(response) {
     return response;
   },
   async onRejected(error) {
-    return Promise.reject(error);
+    const httpClient = (this as any).instance;
+    const axiosClient = httpClient.axiosInstance;
+
+    const {
+      config: originConfig,
+      response: { data },
+    } = error;
+
+    if (data.message !== "jwt expired") return Promise.reject(error);
+
+    const token = localStorage.getItem("token");
+    if (!token) return Promise.reject(error);
+
+    if (!isRefreshing) {
+      console.log("in");
+      httpClient
+        .put("user/token")
+        .then((res) => {
+          console.log(res);
+        })
+        .catch(() => {
+          console.log("error");
+        });
+    }
   },
 };
 
